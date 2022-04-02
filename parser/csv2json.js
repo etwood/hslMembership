885e52dd7ce1a793d2617e258eab1296caf34e64
Eric Wood <eric.5385@gmail.com>
2022-04-02 08:43:11 -0700

    Combined members and amounts

--- parser/csv2json.js
+++ parser/csv2json.js
@@ -1,37 +1,45 @@
 const csv = require('csv-parser')
 const fs = require('fs')
-const results = {};
+const results = [];
 const dates = [];
 const associateAmount = [];
 const basicAmount = [];
-const totalAmount = [];
+const otherAmount = [];
 const associateCount = [];
 const basicCount = [];
-const totalCount = [];
+const otherCount = [];
 
 function addMonthToHighchartsArrays(data) {
   const year = parseInt(data.dateNumeric.substr(0,4));
   const month = parseInt(data.dateNumeric.substr(5,2));
   dates.push(new Date(year, month, 1));
-  associateAmount.push(parseFloat(data.associateAmount.substr(1)));
-  basicAmount.push(parseFloat(data.basicAmount.substr(1)));
-  totalAmount.push(parseFloat(data.totalAmount.substr(1)));
-  associateCount.push(parseInt(data.associateCount));
-  basicCount.push(parseInt(data.basicCount));
-  totalCount.push(parseInt(data.totalCount));
+
+  const fAssociateAmount = parseFloat(data.associateAmount.substr(1));
+  const fBasicAmount = parseFloat(data.basicAmount.substr(1));
+  const fTotalAmount = parseFloat(data.totalAmount.substr(1));
+  associateAmount.push(fAssociateAmount);
+  basicAmount.push(fBasicAmount);
+  otherAmount.push(fTotalAmount - fAssociateAmount - fBasicAmount);
+
+  const iAssociateCount = parseInt(data.associateCount);
+  const iBasicCount = parseInt(data.basicCount);
+  const iTotalCount = parseInt(data.totalCount);
+  associateCount.push(iAssociateCount);
+  basicCount.push(iBasicCount);
+  otherCount.push(iTotalCount - iAssociateCount - iBasicCount);
 }
 
 fs.createReadStream('monthlyTotals.csv')
   .pipe(csv())
   .on('data', (data) => addMonthToHighchartsArrays(data))
   .on('end', () => {
-    results.dates = dates;
-    results.associateAmount = associateAmount;
-    results.basicAmount = basicAmount;
-    results.totalAmount = totalAmount;
-    results.associateCount = associateCount;
-    results.basicCount = basicCount;
-    results.totalCount = totalCount;
+    results.push({name: 'dates', data: dates});
+    results.push({name: 'associateAmount', data: associateAmount});
+    results.push({name: 'basicAmount', data: basicAmount});
+    results.push({name: 'otherAmount', data: otherAmount});
+    results.push({name: 'associateCount', data: associateCount});
+    results.push({name: 'basicCount', data: basicCount});
+    results.push({name: 'otherCount', data: otherCount});
     
     console.log(results);
     fs.writeFile('monthlyTotals.json', JSON.stringify(results, null, 2), err => {
