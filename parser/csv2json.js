51b641529a439541703f4a1bd76a60c7f202aa34
Eric Wood <eric.5385@gmail.com>
2022-04-02 07:28:59 -0700

    Make Highcharts arrays

--- parser/csv2json.js
+++ parser/csv2json.js
@@ -1,11 +1,38 @@
 const csv = require('csv-parser')
 const fs = require('fs')
 const results = [];
+const dates = [];
+const associateAmount = [];
+const basicAmount = [];
+const totalAmount = [];
+const associateCount = [];
+const basicCount = [];
+const totalCount = [];
+
+function addMonthToHighchartsArrays(data) {
+  const year = parseInt(data.dateNumeric.substr(0,4));
+  const month = parseInt(data.dateNumeric.substr(5,2));
+  dates.push(new Date(year, month, 1));
+  associateAmount.push(data.associateAmount);
+  basicAmount.push(data.basicAmount);
+  totalAmount.push(data.totalAmount);
+  associateCount.push(data.associateCount);
+  basicCount.push(data.basicCount);
+  totalCount.push(data.totalCount);
+}
 
 fs.createReadStream('monthlyTotals.csv')
   .pipe(csv())
-  .on('data', (data) => results.push(data))
+  .on('data', (data) => addMonthToHighchartsArrays(data))
   .on('end', () => {
+    results.dates = dates;
+    results.associateAmount = associateAmount;
+    results.basicAmount = basicAmount;
+    results.totalAmount = totalAmount;
+    results.associateCount = associateCount;
+    results.basicCount = basicCount;
+    results.totalCount = totalCount;
+    
     console.log(results);
     // [
     //   { NAME: 'Daffy Duck', AGE: '24' },
