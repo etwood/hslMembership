470506547da67963f72468200966ed50b28ca1e1
Eric Wood <eric.5385@gmail.com>
2022-04-02 07:56:47 -0700

    Move gitignore back to root

--- parser/csv2json.js
+++ parser/csv2json.js
@@ -1,6 +1,6 @@
 const csv = require('csv-parser')
 const fs = require('fs')
-const results = [];
+const results = {};
 const dates = [];
 const associateAmount = [];
 const basicAmount = [];
@@ -13,12 +13,12 @@ function addMonthToHighchartsArrays(data) {
   const year = parseInt(data.dateNumeric.substr(0,4));
   const month = parseInt(data.dateNumeric.substr(5,2));
   dates.push(new Date(year, month, 1));
-  associateAmount.push(data.associateAmount);
-  basicAmount.push(data.basicAmount);
-  totalAmount.push(data.totalAmount);
-  associateCount.push(data.associateCount);
-  basicCount.push(data.basicCount);
-  totalCount.push(data.totalCount);
+  associateAmount.push(parseFloat(data.associateAmount.substr(1)));
+  basicAmount.push(parseFloat(data.basicAmount.substr(1)));
+  totalAmount.push(parseFloat(data.totalAmount.substr(1)));
+  associateCount.push(parseInt(data.associateCount));
+  basicCount.push(parseInt(data.basicCount));
+  totalCount.push(parseInt(data.totalCount));
 }
 
 fs.createReadStream('monthlyTotals.csv')
@@ -34,8 +34,11 @@ fs.createReadStream('monthlyTotals.csv')
     results.totalCount = totalCount;
     
     console.log(results);
-    // [
-    //   { NAME: 'Daffy Duck', AGE: '24' },
-    //   { NAME: 'Bugs Bunny', AGE: '22' }
-    // ]
+    fs.writeFile('monthlyTotals.json', JSON.stringify(results, null, 2), err => {
+      if (err) {
+        console.error(err)
+        return
+      }
+    })
+    
   });
